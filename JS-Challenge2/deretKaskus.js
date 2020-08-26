d2dfdd6eaf2e858da065e4a6cba7fc164f70efce
YudhitiraM <ma.yudhstira@gmail.com>
2020-08-26 15:40:06 +0700

    additional challenge2

--- JS-Challenge2/deretKaskus.js
+++ JS-Challenge2/deretKaskus.js
@@ -1,10 +1,17 @@
 function deretKaskus (n){
   let result = [];
   for(let i = 3; i <= n * 3; i +=3){
-    // if(){
-console.log('Test')
-    // }
+    if(i % 5 === 0 && i % 6 === 0){
+      result.push("KASKUS")
+    } else if (i % 6 === 0){
+      result.push("KUS")
+    } else if (i % 5 === 0){
+      result.push("KAS")
+    } else {
+      result.push(i)
+    }
   }
+  return result;
 }
 
-deretKaskus(2)
+console.log(deretKaskus(10))
