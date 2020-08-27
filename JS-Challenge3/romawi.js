d8452724881c524e0f9fee325330d7117d6f4a9a
YudhitiraM <ma.yudhstira@gmail.com>
2020-08-27 12:45:13 +0700

    add challenge3

---
+++ JS-Challenge3/romawi.js
@@ -0,0 +1,14 @@
+function romawi(n){
+
+}
+
+console.log("Script Testing untuk Konversi Romawi\n")
+console.log("input | expected | result");
+console.log("_____ | ________ | ______");
+console.log("4     | IV       | romawi(4)");
+console.log("9     | IX       | romawi(9)");
+console.log("13    | XIII     | romawi(13)");
+console.log("1453  | MCDLIII  | romawi(1453)");
+console.log("1646  | MCDXLVI  | romawi(1646)");
+
+
