6f7b26a6bf1d90407057fab2f90e7f9d16e331a6
YudhitiraM <ma.yudhstira@gmail.com>
2020-08-27 13:37:49 +0700

    additional challenge3

--- JS-Challenge3/romawi.js
+++ JS-Challenge3/romawi.js
@@ -1,14 +1,26 @@
-function romawi(n){
+function romawi(n) {
+var desimal = [1000,900,500,400,100,90,50,40,10,9,5,4,1];
+var roman = ["M","CM","C","CD", "C","XC","L","XL","X","IX","V","IV","I"];
+var hasil = "";
 
+
+if (n <= 0 || n >= 4000)
+return n;
+
+for (var i = 0; i < desimal.length; i++){
+  while (n % desimal[i] < n) {
+    hasil += roman [i];
+    n -= desimal [i];
+  }
+}
+return hasil;
 }
 
-console.log("Script Testing untuk Konversi Romawi\n")
+console.log("Script Testing untuk Konversi Romawi\n");
 console.log("input | expected | result");
 console.log("_____ | ________ | ______");
-console.log("4     | IV       | romawi(4)");
-console.log("9     | IX       | romawi(9)");
-console.log("13    | XIII     | romawi(13)");
-console.log("1453  | MCDLIII  | romawi(1453)");
-console.log("1646  | MCDXLVI  | romawi(1646)");
-
-
+console.log("0     | 0        |", romawi(0));
+console.log("9     | IX       |", romawi(9));
+console.log("13    | XIII     |", romawi(13));
+console.log("1453  | MCDLIII  |", romawi(1453));
+console.log("1646  | MDCXLVI  |", romawi(1646));
