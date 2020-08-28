79febd9ee4702f3a274f61a01e7c2f95639ce825
YudhitiraM <ma.yudhistira@hotmail.com>
2020-08-28 16:53:39 +0700

    addtional challenge4

--- JS-Challenge4/indexPrime.js
+++ JS-Challenge4/indexPrime.js
@@ -1,5 +1,24 @@
 function indexPrime(param1){
-
+ function cekPrime(param1){
+ 
+ let bilanganPrima = Math.sqrt(param1);
+ 
+ for(let i = 2; i <= bilanganPrima; i++){
+  if(param1 % i === 0)
+  return false;
+  }
+  return true;
+ }
+ 
+ let mulai = 2;
+ let count = 0;
+ while(count != param1){
+  if(cekPrime(mulai)){
+   count++;
+  }
+  mulai++;
+ }
+ return mulai - 1
 }
 
 console.log(indexPrime(4))
