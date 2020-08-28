9a5518a47bbdb40e44c87005981ae7be2a7d45ae
YudhitiraM <ma.yudhistira@hotmail.com>
2020-08-28 17:14:12 +0700

    addtional update challenge5

--- JS-Challenge5/stringManipulation.js
+++ JS-Challenge5/stringManipulation.js
@@ -1,5 +1,21 @@
 function stringManipulation(word){
+ let str1 ="";
+ let str2 ="";
+ let str3 ="";
+
+ if(word.charAt(0) == 'a' || word.charAt(0) == 'A'){
+ return word;
+ } else if (word.charAt(0) == 'i' || word.charAt(0) == 'I'){
+   return word;
+ } else if(){
  
+ } else if(){
+
+ } else if(){
+ 
+ } else {
+ 
+ }
 }
 
 stringManipulation('ayam');
