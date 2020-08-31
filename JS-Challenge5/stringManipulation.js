30c16c899ed5aac7f13d710154766ae9866274f5
YudhitiraM <ma.yudhistira@hotmail.com>
2020-08-31 11:30:05 +0700

    update finall challenge5

--- JS-Challenge5/stringManipulation.js
+++ JS-Challenge5/stringManipulation.js
@@ -1,24 +1,25 @@
-function stringManipulation(word){
- let str1 ="";
- let str2 ="";
- let str3 ="";
+function stringmanipulation(word){
+var str1 = "";
+var str2 = "";
+var str3 = "";
 
- if(word.charAt(0) == 'a' || word.charAt(0) == 'A'){
- return word;
- } else if (word.charAt(0) == 'i' || word.charAt(0) == 'I'){
-   return word;
- } else if(word.charAt(0) == 'u' || word.charAt(0) == 'U'){
-   return word;
- } else if(word.charAt(0) == 'e' || word.charAt(0) == 'E'){
-   return word;
- } else if(word.charAt(0) == 'o' || word.charAt(0) == 'O'){
-   return word;
- } else {
-   str1 = word.slice(0,1);
-   str2 = word.slice(1);
-   str3 = 'nyo';
- }
+if (word.charAt(0) == 'a' || word.charAt(0) == 'A') {
+return word;
+} else if (word.charAt(0) == 'i' || word.charAt(0) == 'I'){
+  return word;
+} else if (word.charAt(0) == 'u' || word.charAt(0) == 'U'){
+  return word;
+} else if (word.charAt(0) == 'e' || word.charAt(0) == 'E'){
+  return word;
+} else if (word.charAt(0) == 'o' || word.charAt(0) == 'O') {
+    return word;
+} else {
+  str1 = word.slice(0,1);
+  str2 = word.slice(1);
+  str3 = 'nyo';
+  return str2 + str1 + str3;
+  }
 }
 
-stringManipulation('ayam');
-stringManipulation('bebek');
+console.log(stringmanipulation('ayam'));
+console.log (stringmanipulation('bebek'));
