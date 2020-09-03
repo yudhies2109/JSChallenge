a3cdc4fbe7625bf5dcdbcfd8d55f749807c71b79
YudhitiraM <ma.yudhistira@hotmail.com>
2020-09-03 13:40:23 +0700

    add challenge7

--- JS-Challenge7/weirdMultiply.js
+++ JS-Challenge7/weirdMultiply.js
@@ -1,7 +1,12 @@
 function weirdMultiply(sentence){
- 
+ let hasil = 1;
+ strAngka = sentence.toString();
+
+ if (strAngka.length == 1){
+  console.log('Dijit Angka 1', strAngka );
+ } else {
+  console.log('Dijit Angka 2', strAngka.charAt(2));
+ }
 }
 
-console.log(stringManipulation(39));
-console.log(stringManipulation(999));
-console.log(stringManipulation(3));
+weirdMultiply(39);
