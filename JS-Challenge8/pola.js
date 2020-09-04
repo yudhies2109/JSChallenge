12c346106f7351fa0ecbecb002cd984b4d23db25
YudhitiraM <ma.yudhistira@hotmail.com>
2020-09-04 11:14:35 +0700

    update challenge8

--- JS-Challenge8/pola.js
+++ JS-Challenge8/pola.js
@@ -1,18 +1,29 @@
 function pola(str) {
+//nb : '*' dan '=' bersifat unflexible dalam 'soal'
   let pecahan1 = str.split("=");
+  //memecah data string menjadi dua bagian (newarray) berdasarkan 'sama dengan'
   let angka3 = pecahan1[1].trim();
+  //'.trim()' menghapus white blank
   let pecahan2 = pecahan1[0].split("*");
+  //memecah data string menjadi dua bagian kembali (newarray) berdasarkan 'operator'
   let angka1 = pecahan2[0].trim();
   let angka2 = pecahan2[1].trim();
-  console.log(pecahan1);
-  console.log(angka3);
-  console.log(pecahan2);
-  console.log(angka1);
-  console.log(angka2);
 
+  for(let i = 0; i < 10; i++){
+   //pengulangan untuk pengecekan pada 'angka1'
+   for(let j = 0; j < 10; j++){
+    //pengulangan untuk pengecekan pada 'angka2'
+    if(parseInt(angka1.replace("#", i)) * 
+    parseInt(angka2) ==
+    parseInt(angka3.replace("#",j)))
+    { 
+     return [i, j];
+    }
+   }
+  }
 }
 
 
-pola("42#3 * 188 = 80#204");
-//console.log(pola("8#61 * 895 = 78410#5"));
+console.log(pola("42#3 * 188 = 80#204"));
+console.log(pola("8#61 * 895 = 78410#5"));
 
